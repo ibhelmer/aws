18b6019e9aa2a6ff9178d1f5ed35b2c45abce9f1
Ib Helmer Nielsen <ihn@ihn.dk>
2024-12-16 23:17:26 +0100

    Tilføjet filehåndtering og RTC

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 
 from machine import Pin, SoftI2C
 from machine_i2c_lcd import I2cLcd
+from machine import SDCard
+from machine import RTC
 from time import sleep
 import network
+import os
 
 # Define the LCD I2C address and dimensions
 I2C_ADDR = 0x27
@@ -29,9 +32,14 @@ def do_connect():
 
 def setup_tcn75(adr):
     i2c.writeto_mem(adr, 0x01, b'\x60')
-def setup_SD():
-    machine.SDCard(slot=1, width=1, cd=None, wp=None, sck=None, miso=None, mosi=None, cs=None, freq=20000000)
-    
+
+def setup_sdcard():
+    # Initialisering af SD-kortet slot #1
+    sd = SDCard(slot=1)  
+    # Montering af SD-kort som et filsystem
+    vfs = os.VfsFat(sd)
+    os.mount(vfs, "/sd")
+
 def read_temp(adr):
     data = i2c.readfrom_mem(adr, 0x00, 2)
     # Convert the data to 12-bits
@@ -46,21 +54,32 @@ def read_temp(adr):
 i2c = i2c = SoftI2C(sda=Pin(21), scl=Pin(22), freq=400000)
 # Initialize LCD and the two Temp sensors
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+rtc=RTC()
+
 setup_tcn75(I2C_TMP1_ADDR)
 setup_tcn75(I2C_TMP2_ADDR)
+setup_sdcard()
 
 lcd.clear()
 do_connect()
 try:
     while True:
-        sleep(2)
+        sleep(10)
         lcd.move_to(0,1)
-        lcd.putstr(str(read_temp(I2C_TMP1_ADDR)))
+        temp1 = read_temp(I2C_TMP1_ADDR)
+        temp2 = read_temp(I2C_TMP2_ADDR)
+        lcd.putstr(str(temp1))
         lcd.move_to(8,1)
-        lcd.putstr(str(read_temp(I2C_TMP2_ADDR)))
+        lcd.putstr(str(temp2))
+        with open("/sd/tempdiff.csv", "a") as file:
+            file.write(str(temp2-temp1)+"\n")
         
 except KeyboardInterrupt:
     # Turn off the display
     print("Keyboard interrupt")
     lcd.backlight_off()
     lcd.display_off()
+    # Afmonter SD-kortet, når det ikke længere skal bruges
+    os.umount("/sd")
+    print("SD-kort unmontet.")
